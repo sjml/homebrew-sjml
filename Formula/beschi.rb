b52beb76b92fc2b3dc6bf057233e7d3446f891dc
Shane Liesegang <shane@techie.net>
2023-01-18 07:37:12 -0500

    seeing if I can make beschi into a formula

---
+++ Formula/beschi.rb
@@ -0,0 +1,14 @@
+class Beschi < Formula
+  include Language::Python::Virtualenv
+
+  desc "Bit-packing and unpacking code generator for multiple target languages"
+  homepage "https://github.com/sjml/beschi"
+  url "https://github.com/sjml/beschi/archive/refs/tags/v0.1.12.tar.gz"
+  sha256 "b771dd8f54a96b8a342532f5f76cfd5ddf485cd3781ec37f1f793e0affddc0dd"
+  license "MIT"
+
+  resource "toml" do
+    url "https://files.pythonhosted.org/packages/be/ba/1f744cdc819428fc6b5084ec34d9b30660f6f9daaf70eead706e3203ec3c/toml-0.10.2.tar.gz"
+    sha256 "b3bda1d108d5dd99f4a20d24d9c348e91c4db7ab1b749200bded2f839ccbe68f"
+  end
+end
