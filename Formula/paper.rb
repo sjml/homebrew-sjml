7fb6f471b984151a55e18082c28400b336ef8341
sjml <sjml@users.noreply.github.com>
2023-02-08 21:08:54 +0000

    Updating paper to v0.5.3

--- Formula/paper.rb
+++ Formula/paper.rb
@@ -1,8 +1,8 @@
 class Paper < Formula
   desc "Shane's little paper-{writing|managing|building} utility"
   homepage "https://github.com/sjml/paper"
-  url "https://github.com/sjml/paper/archive/refs/tags/v0.5.2.tar.gz"
-  sha256 "6c40fd04f109d3651dc3c42bd60046ff93807f561c5f3f02eef1d9d4d3618022"
+  url "https://github.com/sjml/paper/archive/refs/tags/v0.5.3.tar.gz"
+  sha256 "11276e6285949504e28095de04853567c80cb61200409a6bc32d0bfacb4fcc54"
   license "MIT"
 
   depends_on "rust" => :build
