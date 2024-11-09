78c9e7a168737004fcadcd59bb93d6184e541788
Shane Liesegang <shane@techie.net>
2024-11-09 11:24:25 +0800

    new min-python for beschi

--- Formula/beschi.rb
+++ Formula/beschi.rb
@@ -7,7 +7,7 @@ class Beschi < Formula
   sha256 "b771dd8f54a96b8a342532f5f76cfd5ddf485cd3781ec37f1f793e0affddc0dd"
   license "MIT"
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   resource "toml" do
     url "https://files.pythonhosted.org/packages/be/ba/1f744cdc819428fc6b5084ec34d9b30660f6f9daaf70eead706e3203ec3c/toml-0.10.2.tar.gz"
