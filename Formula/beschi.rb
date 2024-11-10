1f81555a5b4724393ee178bafc8327247bf10d76
Shane Liesegang <shane@techie.net>
2024-11-10 21:40:59 +0800

    beschi bump

--- Formula/beschi.rb
+++ Formula/beschi.rb
@@ -3,16 +3,11 @@ class Beschi < Formula
 
   desc "Bit-packing and unpacking code generator for multiple target languages"
   homepage "https://github.com/sjml/beschi"
-  url "https://github.com/sjml/beschi/archive/refs/tags/v0.1.12.tar.gz"
-  sha256 "b771dd8f54a96b8a342532f5f76cfd5ddf485cd3781ec37f1f793e0affddc0dd"
+  url "https://github.com/sjml/beschi/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "bef6035a66b2ab22196986c77b1a140c4ff769c158b1dfcb59abcc561c2d956e"
   license "MIT"
 
   depends_on "python@3.11"
-
-  resource "toml" do
-    url "https://files.pythonhosted.org/packages/be/ba/1f744cdc819428fc6b5084ec34d9b30660f6f9daaf70eead706e3203ec3c/toml-0.10.2.tar.gz"
-    sha256 "b3bda1d108d5dd99f4a20d24d9c348e91c4db7ab1b749200bded2f839ccbe68f"
-  end
 
   def install
     virtualenv_install_with_resources
