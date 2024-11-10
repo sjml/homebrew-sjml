174e121658a2a62cb6ccf8a15622860989da4d07
Shane Liesegang <shane@techie.net>
2024-11-10 22:34:32 +0800

    fixing beschi sha

--- Formula/beschi.rb
+++ Formula/beschi.rb
@@ -4,7 +4,7 @@ class Beschi < Formula
   desc "Bit-packing and unpacking code generator for multiple target languages"
   homepage "https://github.com/sjml/beschi"
   url "https://github.com/sjml/beschi/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "bef6035a66b2ab22196986c77b1a140c4ff769c158b1dfcb59abcc561c2d956e"
+  sha256 "19198d1043b4e3f37791fa0fd5e0f27588f30b188a05ff7f0b1a7a2923b020a6"
   license "MIT"
 
   depends_on "python@3.11"
