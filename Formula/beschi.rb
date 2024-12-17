a8c9c0d6fa187bcc8c3aa97141562030d122d9b5
sjml <sjml@users.noreply.github.com>
2024-12-17 06:29:37 +0000

    Updating beschi to v0.3.0

--- Formula/beschi.rb
+++ Formula/beschi.rb
@@ -3,8 +3,8 @@ class Beschi < Formula
 
   desc "Bit-packing and unpacking code generator for multiple target languages"
   homepage "https://github.com/sjml/beschi"
-  url "https://github.com/sjml/beschi/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "c5dbcbd5394865330ef3d7ecf507ba36e94b6f1b4a84b9b03ed97ff6041acacf"
+  url "https://github.com/sjml/beschi/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "d269eba5b1d35dbb2cb43c6728e0b4429a1bbc7ee88d0cb9a90c9738c0fdf375"
   license "MIT"
 
   depends_on "python@3.11"
