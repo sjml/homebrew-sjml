87a23b6114961dc555a89b84fc4810dfe13f8ab7
sjml <sjml@users.noreply.github.com>
2025-06-14 18:39:16 +0000

    Updating hdr-resize to 0.1.2

--- Formula/hdr-resize.rb
+++ Formula/hdr-resize.rb
@@ -1,8 +1,8 @@
 class HdrResize < Formula
   desc "Resizes HDR images in JPEG or HEIC format while retaining their dynamic range"
   homepage "https://github.com/sjml/hdr-resize"
-  url "https://github.com/sjml/hdr-resize/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "ad0cefb15a125b914b94165537b6c32ed5ff49b0e24a1a29affa0e8c5ad47808"
+  url "https://github.com/sjml/hdr-resize/archive/refs/tags/0.1.2.tar.gz"
+  sha256 "706476c8f5f5dc088d3bb5f7d5ed0f6bf66638a5207e2432c365e2e1fe33dcdb"
   license "MIT"
 
   def install
